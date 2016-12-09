6e9078543dbc734f618249aa0200e91c6b567d23
gaoyx <melo@gaoyuexiang.cn>
2016-12-09 14:28:02 +0800

    add LoginResult model

---
+++ src/main/java/cn/gaoyuexiang/rmi/test/model/LoginResult.java
@@ -0,0 +1,40 @@
+package cn.gaoyuexiang.rmi.test.model;
+
+import java.io.Serializable;
+
+public class LoginResult implements Serializable {
+
+	private static final long serialVersionUID = 1245372725497797920L;
+
+	private Long id;
+
+	private String token;
+
+	private String status;
+
+	public LoginResult() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
